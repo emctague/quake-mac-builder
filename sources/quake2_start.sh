92e79fa73696b64002bbc50c152233b5c4314935
Ethan McTague <ethan@ayapayments.com>
2021-01-07 11:21:30 -0500

    Add simple prompt to startup scripts

--- sources/quake2_start.sh
+++ sources/quake2_start.sh
@@ -8,4 +8,10 @@ export VK_LAYER_PATH="$VULKAN_SDK"/vulkansdk/macOS/share/vulkan/explicit_layer.d
 mkdir -p "$HOME/Library/Application Support/Quake II/baseq2"
 cd "$HOME/Library/Application Support/Quake II"
 
+if [ ! -f "baseq2/pak0.pak" ]; then
+	osascript -e 'tell app "System Events" to display dialog "To play Quake 2, copy all .pak files from your official Quake II disk'\''s baseq2 folder into this app'\''s baseq2 folder" buttons {"Open baseq2 Folder"} default button 1'
+	open -a Finder ./baseq2
+	exit
+fi
+
 exec "$CORE/quake2"
--- sources/quake_start.sh
+++ sources/quake_start.sh
@@ -5,5 +5,12 @@
 CORE="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 mkdir -p "$HOME/Library/ApplicationSupport/Quake/id1"
 cd "$HOME/Library/ApplicationSupport/Quake"
-exec "$CORE/QuakeSpasm" -basedir "$PWD"
 
+
+if [ ! -f "id1/PAK0.PAK" ]; then
+	osascript -e 'tell app "System Events" to display dialog "To play Quake, copy all .PAK files from your official Quake disk'\''s id1 folder into this app'\''s id1 folder" buttons {"Open id1 Folder"} default button 1'
+	open -a Finder ./id1
+	exit
+fi
+
+exec "$CORE/QuakeSpasm" -basedir "$PWD"
